32f09aa2debd824eddde31375ef17471d02e3bb7
Diana <d_quinlan061006@yahoo.com>
2019-07-08 18:23:25 -0400

    deploy test 2, removed clicky folder

---
+++ build/precache-manifest.c3ad3c0108f1d21262a1f887e8e98944.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa9541a216bc71cbeb83bf9f3eb3870",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "9be1a11cb3781b61c948",
+    "url": "/Clicky-Game/static/css/main.36d0dba2.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "9be1a11cb3781b61c948",
+    "url": "/Clicky-Game/static/js/main.b023782a.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
