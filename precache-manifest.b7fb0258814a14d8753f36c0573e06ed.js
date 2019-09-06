069dbcd1cc192597b3980ab74d5d8b236667c684
Diana <d_quinlan061006@yahoo.com>
2019-09-06 17:57:39 -0400

    Updates

---
+++ precache-manifest.b7fb0258814a14d8753f36c0573e06ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a57545b1d5be71d2147edc5bbf056da",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "6bfec678de2f0cda6223",
+    "url": "/Clicky-Game/static/css/main.52f0c176.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "6bfec678de2f0cda6223",
+    "url": "/Clicky-Game/static/js/main.538074a0.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
